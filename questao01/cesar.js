75cde35ef39bbe3f8095b08a9026ba23705863d1
Savio Henrique <saviobarradas@gmail.com>
2024-11-19 20:35:48 -0300

    fixed cesar.js

--- questao01/cesar.js
+++ questao01/cesar.js
@@ -1,53 +1,39 @@
-let alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const alfa = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function criptografia(mensagem, chave, converter){
-    let m = mensagem
     let mConvertida = []
 
-    if(converter == c){
-
-        for(let i = 0; i <= m.length; i++){
-            if(m[i] != " "){
-                mConvertida[i] = cifraLetra(m[i], chave) 
+    for(let i = 0; i < mensagem.length; i++){
+        if(mensagem[i] !== " "){
+            if(converter == "c"){
+                mConvertida[i] = cifraLetra(mensagem[i], chave)
             }
-            else{
-                mConvertida[i] = " "
+            else if(converter == "d"){
+                mConvertida[i] = decifraLetra(mensagem[i], chave)
             }
+        }   
+        else{
+            mConvertida[i] = " "
         }
-    
     }
-    else if(converter == d){
+    return mConvertida.join("")
 
-        for(let i = 0; i <= m.length; i++){
-            if(m[i] != " "){
-                mConvertida[i] = decifraLetra(m[i], chave) 
-            }
-            else{
-                mConvertida[i] = " "
-            }
-        }
-    
-    }
-    
 }
 
 function cifraLetra(letra, chave){
-    for(let i; i <= alfa.length; i++){
-        if(letra == alfa[i] && (i + chave) <= 25){
-            return alfa[i + chave]
+    for(let i = 0; i < alfa.length; i++){
+        if(letra === alfa[i]){
+            let novoI = (i + chave) % 26
+            return alfa[novoI]
         }
-        else if(letra == alfa[i] && (i + chave) > 25)
-            return alfa[(i + chave) - 26]
     }
 }
 
 function decifraLetra(letra, chave){
-    for(let i; i <= alfa.length; i++){
-        if(letra == alfa[i] && (i - chave) >= 0){
-            return alfa[i - chave]
-        }
-        else if(letra == alfa[i] && (i - chave) < 0){
-            return alfa[26 - (i - chave)]
+    for(let i = 0; i < alfa.length; i++){
+        if(letra === alfa[i]){
+            let novoI = (i - chave + 26) % 26
+            return alfa[novoI]
         }
     }
 }
--- questao01/index.js
+++ questao01/index.js
@@ -1,5 +1,11 @@
 import cs from "./cesar.js"
 
-var m = ["a"]
+const mensagem = "ola mundo";
+const mensagemCript = "rood qxpgr"
+const chave = 3;
 
-console.log(cs.criptografia("a", 3))
+const criptografada = cs.criptografia(mensagem, chave, "c");
+console.log("Criptografada:", criptografada); // Saída esperada: "rod pxqgr"
+
+const decifrada = cs.criptografia(mensagemCript, chave, "d");
+console.log("Decifrada:", decifrada); // Saída esperada: "ola mundo"
